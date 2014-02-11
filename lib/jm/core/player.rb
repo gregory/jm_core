32ff1f8f47cb2ed5b59f7f3adba24600202421d7
gregory <greg2502@gmail.com>
2014-02-10 16:07:57 -0800

    add player domain

---
+++ lib/jm/core/player.rb
@@ -0,0 +1,32 @@
+module Jm
+  module Core
+    class Player
+      include Virtus.model
+      include Wisper::Publisher
+
+      attribute :uuid, String
+      attribute :name, String
+      attribute :nick, String
+
+      def self.create(params)
+        player = self.new
+        params = params.merge(uuid: new_uuid)
+
+        player.player_created params
+        player.send(:publish, :player_created, params)
+
+        player
+      end
+
+      def player_created(event)
+        self.attributes = event
+      end
+
+      private
+
+      def self.new_uuid
+        UUIDTools::UUID.timestamp_create.to_s
+      end
+    end
+  end
+end
